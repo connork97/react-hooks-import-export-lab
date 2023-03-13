60388005b9e291f3ef67019576d5e1a2e2074502
Connor Kormos <connorkormos97@gmail.com>
2023-03-13 10:51:24 -0700

    complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
+import * as RMFunc from '../data/user';
 
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {RMFunc.username} is a Web Developer from {RMFunc.city}
       </h1>
     </div>
   );
 }
+
+export default Home;
